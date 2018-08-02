f08d654440692cc1b89cbf7cc501e17a25e6fbbe
nenew121 <33708352+nenew121@users.noreply.github.com>
2018-08-02 09:05:24 +0700

    Add files via upload

--- Code2.php
+++ Code2.php
@@ -28,17 +28,25 @@ function Leaveinformation($LineID,$Status){
     $url = "http://lineservice.prosofthcm.com/api/LeaveinformationAPI/".$LineID."/".$Status;
     $open = json_decode(file_get_contents($url), true);
     $arr = [];
+    $sum = "";
     if($open != null){
         foreach($open as $text){
-            array_push($arr,("วันที่ลา :".$text['DocuDate']."\n ประเภทการลา :".$text['LeaveTypeName']."\n จำนวนวันลา : 1"."\n สาเหตุการลา :".$text['LeaveRemark']."\n--------------------------------\n"));
+            array_push($arr,("วันที่ลา :".$text['DocuDate']."\n"));
+            array_push($arr,("ประเภทการลา :".$text['LeaveTypeName']."\n"));
+            array_push($arr,("จำนวนวันลา : 1"."\n"));
+            array_push($arr,("สาเหตุการลา :".$text['LeaveRemark']."\n"));
+            array_push($arr,("--------------------------------\n"));
+        }
+        for($i=0;$i<count($arr);$i++){
+            $sum = $sum.$arr[$i];
         }
     }else{
         $arr[0] = "ไม่มีพบข้อมูล";
         return $arr;
     }
 
-
-    return $arr;
+    
+    return $sum;
 }
 
 ?>
