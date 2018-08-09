3fff4f936b0c3d594bc7e6fc31ee7dc1198e488b
nenew121 <33708352+nenew121@users.noreply.github.com>
2018-08-09 11:48:59 +0700

    Add files via upload

--- Code3.php
+++ Code3.php
@@ -52,8 +52,9 @@ class BOT_API extends LINEBot
     public $isImage         = false;
     public $isSticker       = false;
     public $isSticker       = false;
-    public $isImagemap       = false;
-
+    public $isImagemap      = false;
+    public $isMessage       = false;
+    
     public $text            = null;
     public $replyToken      = null;
     public $source          = null;
@@ -103,6 +104,10 @@ class BOT_API extends LINEBot
 
                 if ($event['type'] == 'message' && $event['message']['type'] == 'imagemap') {
                     $this->isImagemap = true;
+                }
+
+                if ($event['type'] == 'message' && $event['message']['type'] == 'message') {
+                    $this->isMessage = true;
                 }
             }
         }
