c7a1897f17bd64697678cf72e257d1b7146c1005
nenew121 <33708352+nenew121@users.noreply.github.com>
2018-08-09 11:32:32 +0700

    Add files via upload

--- Code3.php
+++ Code3.php
@@ -366,8 +366,7 @@ public function pho123($replyToken = null)
 
 
         $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
-            'replyToken' => $replyToken,
-            'messagesasdfasdf'   => $test1->buildMessage(),
+            'replyToken' => $replyToken
         ]);
 }
 
