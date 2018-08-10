f411f80158bed2c0ee59e555031070e8a34103ca
nenew121 <33708352+nenew121@users.noreply.github.com>
2018-08-10 11:41:04 +0700

    Add files via upload

--- Code3.php
+++ Code3.php
@@ -360,6 +360,13 @@ public function BOT_New($replyToken = null, $text)
             'messages'   => $outputText->buildMessage(),
             ]);
         break;
+        case "Q1":
+        $outputText = new ImageMessageBuilder("https://lineservice.prosofthcm.com/upload/Resource/Leave.png", "https://lineservice.prosofthcm.com/upload/Resource/Leave.png");
+        $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
+        'replyToken' => $replyToken,
+        'messages'   => $outputText->buildMessage(),
+        ]);
+    break;
         case "St":
             $replyData = new StickerMessageBuilder("1","17");
             $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
@@ -368,10 +375,10 @@ public function BOT_New($replyToken = null, $text)
             ]);
         break;
         case "Im":
-        $base = new BaseSizeBuilder(699,1040);
+        $base = new BaseSizeBuilder(699,1000);
         $arr = array(
             new ImagemapMessageActionBuilder("tt", new AreaBuilder(0,0,10,10)),
-            new ImagemapMessageActionBuilder("tt", new AreaBuilder(0,0,10,10))
+            new ImagemapMessageActionBuilder("ttt", new AreaBuilder(0,0,10,10))
         );
         $replyData = new ImagemapMessageBuilder("https://lineservice.prosofthcm.com/upload/Resource/Leave.png","test",$base,$arr);
         $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
