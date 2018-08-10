5a34f65fa7ee311cb15ee6b48c44da198ee69863
nenew121 <33708352+nenew121@users.noreply.github.com>
2018-08-10 10:35:01 +0700

    Add files via upload

--- Code3.php
+++ Code3.php
@@ -449,14 +449,16 @@ public function BOT_New($replyToken = null, $text)
           new MessageTemplateActionBuilder("English", "ENG"),
           new MessageTemplateActionBuilder("ยกเลิก(Cancel)", "Cancel"),
         );
+            $column = new CarouselColumnTemplateBuilder("Language", "กรุณาเลือกภาษาทต้องการเปลี่ยน\nPlease select a display language.", $img_url, $actions);
+            $columns[] = $column;
             $ar = array(
                 new CarouselColumnTemplateBuilder("Language", "กรุณาเลือกภาษาทต้องการเปลี่ยน\nPlease select a display language.", $img_url, $actions),
                 new CarouselColumnTemplateBuilder("Language", "กรุณาเลือกภาษาทต้องการเปลี่ยน\nPlease select a display language.", $img_url, $actions);
             );
 
-            $carousel = new CarouselTemplateBuilder($$ar);
-            $outputText = new TemplateMessageBuilder("Setting Language", $carousel);
-            $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
+        $carousel = new CarouselTemplateBuilder($columns);
+        $outputText = new TemplateMessageBuilder("Setting Language", $carousel);
+        $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
                 'replyToken' => $replyToken,
                 'messages'   => $outputText->buildMessage(),
             ]);
