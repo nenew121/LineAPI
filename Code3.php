de2248b467b2d8589e1e3fd46eebf198a9936c34
nenew121 <33708352+nenew121@users.noreply.github.com>
2018-08-10 11:09:22 +0700

    Add files via upload

--- Code3.php
+++ Code3.php
@@ -367,6 +367,18 @@ public function BOT_New($replyToken = null, $text)
             'messages'   => $replyData->buildMessage(),
             ]);
         break;
+        case "im":
+        $base = new BaseSizeBuilder(70,104);
+        $arr = array(
+            new ImageMessageBuilder("https://lineservice.prosofthcm.com/upload/Resource/Linebot.png", "https://lineservice.prosofthcm.com/upload/Resource/Linebot.png"),
+            new ImageMessageBuilder("https://lineservice.prosofthcm.com/upload/Resource/Linebot.png", "https://lineservice.prosofthcm.com/upload/Resource/Linebot.png")
+        );
+        $replyData = new ImagemapMessageBuilder("https://lineservice.prosofthcm.com/upload/Resource/Linebot.png","test",$base,$arr);
+        $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
+        'replyToken' => $replyToken,
+        'messages'   => $replyData->buildMessage(),
+        ]);
+    break;
         case "T1":
             $asd = new AreaBuilder(0,0,50,50);
             $az = new ImagemapUriActionBuilder("https://lineservice.prosofthcm.com/upload/Resource/img.png",$asd);
@@ -463,8 +475,7 @@ public function BOT_New($replyToken = null, $text)
         case "T8":
         
         $img_url = "https://www.prosofthcm.com/upload/5934/eo3hrcpDoM.png";
-        for ($i=0;$i<1;$i++) {
-            $actions = array(
+        $actions = array(
           new MessageTemplateActionBuilder("ภาษาไทย", "TH"),
           new MessageTemplateActionBuilder("English", "ENG"),
           new MessageTemplateActionBuilder("ยกเลิก(Cancel)", "Cancel"),
@@ -473,7 +484,6 @@ public function BOT_New($replyToken = null, $text)
             new CarouselColumnTemplateBuilder("Language", "กรุณาเลือกภาษาทต้องการเปลี่ยน\nPlease select a display language.", $img_url, $actions),
             new CarouselColumnTemplateBuilder("Language", "กรุณาเลือกภาษาทต้องการเปลี่ยน\nPlease select a display language.", $img_url, $actions)
         );
-        }
         $carousel = new CarouselTemplateBuilder($columns);
         $outputText = new TemplateMessageBuilder("Setting Language", $carousel);
         $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
