6131b8e55db4bfc8754726aeeb7c460b7ba1b38a
nenew121 <33708352+nenew121@users.noreply.github.com>
2018-08-17 13:35:45 +0700

    Update img.php

--- img.php
+++ img.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
  
 // include composer autoload
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 // กรณีมีการเชื่อมต่อกับฐานข้อมูล
 //require_once("dbconnect.php");
  
