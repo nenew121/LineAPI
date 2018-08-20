16d04c83ef365f6eb3c1aa454d810bd98f9aaea6
nenew121 <33708352+nenew121@users.noreply.github.com>
2018-08-20 14:23:23 +0700

    Add files via upload

--- Code3.php
+++ Code3.php
@@ -430,10 +430,10 @@ public function BOT_New($replyToken = null, $text)
         case "T1":
             $base = new BaseSizeBuilder(1040,710);
             $arr = array(
-                new ImagemapMessageActionBuilder("Text1", new AreaBuilder(0,925,1040,175)),
-                new ImagemapMessageActionBuilder("Text2", new AreaBuilder(0,750,1040,175)),
-                new ImagemapMessageActionBuilder("Text3", new AreaBuilder(0,575,1040,175)),
-                new ImagemapMessageActionBuilder("Text4", new AreaBuilder(0,400,1040,175))
+                new ImagemapMessageActionBuilder("Text1", new AreaBuilder(0,790,1040,130)),
+                new ImagemapMessageActionBuilder("Text2", new AreaBuilder(0,660,1040,130)),
+                new ImagemapMessageActionBuilder("Text3", new AreaBuilder(0,530,1040,130)),
+                new ImagemapMessageActionBuilder("Text4", new AreaBuilder(0,400,1040,130))
 
                 //new ImagemapUriActionBuilder("https://www.google.co.th", new AreaBuilder(35,624,965,199)),
                 //new ImagemapUriActionBuilder("https://www.google.co.th", new AreaBuilder(35,823,965,186)),
