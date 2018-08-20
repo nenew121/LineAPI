6f8e766e42e1548d613856db5410a16a0634479f
nenew121 <33708352+nenew121@users.noreply.github.com>
2018-08-20 12:02:02 +0700

    Update Code3.php

--- Code3.php
+++ Code3.php
@@ -410,7 +410,7 @@ public function BOT_New($replyToken = null, $text)
         ]);
         break;
         case "T1":
-            $base = new BaseSizeBuilder(1040,699);
+            $base = new BaseSizeBuilder(1040,710);
             $arr = array(
                 new ImagemapUriActionBuilder("https://www.google.co.th", new AreaBuilder(0,0,520,699)),
                 new ImagemapMessageActionBuilder("Text", new AreaBuilder(520,0,520,699))
